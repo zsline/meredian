0aa7c7d5ea31386676467f858707bc4bb4036a54
zsline <sergei.dereka@gmail.com>
2022-08-28 10:21:20 +0300

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,51 @@
 
 import * as flsFunctions from "./modules/functions.js";
-import Swiper, { Navigation, Pagination } from 'swiper';
-
 flsFunctions.isWebp();
 
-const swiper = new Swiper();
 
 
 
+let number = document.querySelectorAll('.furniture__slider-item');
+let images = document.querySelectorAll('.furniture__slider-item img');
+let current = 0;
+// console.log(img);
+document.querySelector('.slider__nav-pagination-all').innerHTML = number.length;
+slider();
+function slider() {
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.add('off');
+
+    }
+    images[current].classList.remove('off');
+    document.querySelector('.slider__nav-pagination-number').innerHTML = current + 1;
+};
+
+
+document.querySelector('.slider__wrapper').onclick = function () {
+    if (current + 1 == images.length) {
+        current = 0;
+    }
+    else {
+        current++;
+    }
+    slider();
+};
+
+document.querySelector('.furniture__slider-prev').onclick = function () {
+    if (current -1 == -1) {
+        current = images.length - 1;
+    }
+    else {
+        current--;
+    }
+    slider();
+};
+document.querySelector('.furniture__slider-next').onclick = function () {
+    if (current + 1 == images.length) {
+        current = 0;
+    }
+    else {
+        current++;
+    }
+    slider();
+};
